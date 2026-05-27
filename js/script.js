c29fcc8f9fe5b26d92139b57d4f36f39a005e5e3
VidaGood <castrojaicasbrocas@gmail.com>
2026-05-26 22:52:52 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -22,7 +22,7 @@ const productos = [
     },
     {
         nombre: "Ejercta tu mente",
-        imagen: "g",
+        imagen: "img/Gemini_Generated_Image_28w3mz28w3mz28w3.png",
         link: "https://go.hotmart.com/E105992330P"
     },
     {
